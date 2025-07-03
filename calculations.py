bb11f65550cbe286bcd4b92a62fa451a981fa59b
Daniel Hyun <dhyun1543@gmail.com>
2025-07-03 17:20:50 -0500

    First commit to main

---
+++ calculations.py
@@ -0,0 +1,48 @@
+from scipy.stats import norm
+import numpy as np
+import math
+
+def black_scholes_price(S, K, T, r, sigma, option_type='call'):
+    d1 = (math.log(S / K) + (r + 0.5 * sigma ** 2) * T) / (sigma * math.sqrt(T))
+    d2 = d1 - sigma * math.sqrt(T)
+
+    if option_type == 'call':
+        price = S * norm.cdf(d1) - K * math.exp(-r * T) * norm.cdf(d2)
+    elif option_type == 'put':
+        price = K * math.exp(-r * T) * norm.cdf(-d2) - S * norm.cdf(-d1)
+    else:
+        raise ValueError("option_type must be 'call' or 'put'")
+
+    return price
+
+def implied_volatility(S, K, T, r, market_price, option_type='call', tol=1e-6, max_iterations=100):
+
+    sigma_low = 1e-6
+    sigma_high = 5.0
+
+    for i in range(max_iterations):
+        sigma_mid = (sigma_low + sigma_high) / 2
+        price = black_scholes_price(S, K, T, r, sigma_mid, option_type)
+        diff = price - market_price
+
+        if abs(diff) < tol:
+            return sigma_mid
+
+        if diff > 0:
+            sigma_high = sigma_mid
+        else:
+            sigma_low = sigma_mid
+
+    return sigma_mid
+
+def calculate_matrix(x_labels: np.array, y_labels: np.array, y_axis: str, realized_volatility: float, K:float, T:float, r:float, option_price: float, option_type:str):
+    matrix=[]
+    for j in y_labels:
+        row=[]
+        for i in x_labels:
+            if "Risk" in y_axis:
+               row.append(implied_volatility(i,K,T,j,option_price, option_type=option_type)-realized_volatility)
+            else:
+               row.append(implied_volatility(i,K,i,r,option_price, option_type=option_type)-realized_volatility)
+        matrix.append(row)
+    return np.array(matrix)
---
+++ main.py
@@ -0,0 +1,80 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+from calculations import calculate_matrix
+
+st.set_page_config(page_title="Volatility Arbitrage Surface", page_icon="icon.png")
+
+st.title("Volatility Arbitrage Surface")
+
+st.sidebar.title("Model Parameters")
+st.sidebar.text("Inputs to get IV (Implied Volatility) from Black Scholes Model")
+strike = st.sidebar.number_input("Strike Price", value=0.00, min_value=0.0)
+time = st.sidebar.number_input("Time to Maturity (Years)", value=0.00, min_value=0.0)
+rate = st.sidebar.number_input("Risk free Rate", min_value=0.00, max_value=1.00)
+op = st.sidebar.number_input("Option Price", min_value=0.00)
+st.sidebar.title("Realized Volatility")
+realized_vol = st.sidebar.number_input("Realized Volatility", value=0.00, min_value=0.0)
+st.sidebar.title("Visual Spot Price Configuration")
+min_spot = st.sidebar.number_input("Min Spot Price", value=0.00, min_value=0.0)
+max_spot = st.sidebar.number_input("Max Spot Price", value=0.00, min_value=0.0)
+st.sidebar.title("Visual Y Axis Configuration")
+st.session_state.y_axis = st.sidebar.selectbox("Select Y-Axis", ["Risk free Rate","Time to Maturity (Years)"])
+min_y = st.sidebar.number_input("Min "+st.session_state.y_axis, value=0.00, min_value=0.0)
+max_y = st.sidebar.number_input("Max "+st.session_state.y_axis, value=0.00, min_value=0.0)
+
+github_url = "https://github.com/danielhyuncoder"
+github_icon_url = "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png"
+
+# Markdown with an image and link
+st.sidebar.markdown(
+    f"""
+    <a href="{github_url}" target="_blank" style="text-decoration: none; display: flex; align-items: center;">
+        <span style="font-weight: bold; margin-right: 8px; color: inherit;">Created by Daniel Hyun</span>
+        <img src="{github_icon_url}" width="20" height="20" alt="GitHub Logo">
+    </a>
+    """,
+    unsafe_allow_html=True
+)
+
+if strike > 0 and time > 0 and rate > 0 and op > 0 and max_spot > 0 and max_y>0 and min_spot>0:
+    fig, ax = plt.subplots()
+    range_x = max_spot - min_spot
+    range_y = max_y - min_y
+    unit_x = max(range_x, 0.00001) / 10
+    unit_y = max(range_y, 0.00001) / 10
+    x_labels = np.round(np.arange(min_spot, max_spot + unit_x, unit_x), 2)
+    y_labels = np.round(np.arange(min_y, max_y + unit_y, unit_y), 2)
+
+    sns.heatmap(
+        calculate_matrix(
+            x_labels, y_labels,
+            st.session_state.y_axis,
+            realized_vol, strike, time, rate, op, "call"
+        ),
+        annot=True, cmap='magma', ax=ax,
+        xticklabels=x_labels, yticklabels=y_labels
+    )
+    ax.set_title("Volatility Arbitrage (IV - RV) for CALL options")
+    ax.set_xlabel("Spot Price")
+    ax.set_ylabel(st.session_state.y_axis)
+    st.pyplot(fig)
+
+    fig2, ax2 = plt.subplots()
+    sns.heatmap(
+        calculate_matrix(
+            x_labels, y_labels,
+            st.session_state.y_axis,
+            realized_vol, strike, time, rate, op, "put"
+        ),
+        annot=True, cmap='magma', ax=ax2,
+        xticklabels=x_labels, yticklabels=y_labels
+    )
+    ax2.set_title("Volatility Arbitrage (IV - RV) for PUT options")
+    ax2.set_xlabel("Spot Price")
+    ax2.set_ylabel(st.session_state.y_axis)
+    st.pyplot(fig2)
+else:
+    st.write("Please input all the fields in order to see the heatmap visualizations.")
